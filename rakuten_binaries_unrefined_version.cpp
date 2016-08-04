bf428c5da4388855a933089a628aa4a81c2534b0
Aziz Latypov <vm06lau@mail.ru>
2016-08-04 16:15:16 +0300

    removed dependency on zeros

--- rakuten_binaries_unrefined_version.cpp
+++ rakuten_binaries_unrefined_version.cpp
@@ -1,28 +1,23 @@
 #include <vector>
 #include <algorithm>
 
-int solution(vector<int> &a) {
-    
-    for(int i=0; i<a.size(); i++) {
-        a[i]++;
-    }
-
+int solution(std::vector<int> &a) {
     bool has_changed = false;
     do {
         std::sort(a.begin(), a.end());
         
         has_changed = false;
-
+        
         for(int i=0; i<a.size()-1; i++) {
-            if(a[i] > 0) {
+            if(a[i] >= 0) {
                 if(a[i] == a[i+1]) {
-                    a[i] = 0;
+                    a[i] = -1;
                     a[i+1]++;
                     has_changed = true;
                 }
             }
         }
-        
+        std::cout << "!";
     } while(has_changed);
     
     int k=0;
